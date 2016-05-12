b31aa5a48662fc4280c84c1538dbee6b3289096d
Jan Keith Darunday <jkcdarunday@gmail.com>
2016-05-12 21:04:04 +0800

    Add support for connecting to Redis with an r2d2 connection pool

--- Server/src/handler.rs
+++ Server/src/handler.rs
@@ -1,9 +1,30 @@
+extern crate redis;
+
 use iron::prelude::*;
 use iron::status;
+
+use persistent::Read;
+
+use database::RedisPool;
+use redis::Commands;
+use redis::Script;
+
 pub struct Enlisted;
 impl Enlisted{
-    pub fn get(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::NotFound, "unimplemented")))
+    pub fn get(req: &mut Request) -> IronResult<Response> {
+        //Ok(Response::with((status::NotFound, "unimplemented")))
+
+        let redis_pool = req.get::<Read<RedisPool>>().unwrap();
+        let redis_connection : &redis::Connection = &*redis_pool.get().unwrap();
+
+        //let n: i64 = redis_connection.incr("counter", 1).unwrap();
+        let script = Script::new("
+            local c = redis.call(\"INCR\", \"counter\");
+            return c/2;
+        ");
+        let n: i32 = script.arg(1).arg(2).invoke(redis_connection).unwrap();
+        //let n = 1;
+        Ok(Response::with((status::Ok, format!("{}", n))))
     }
 
     pub fn put(_: &mut Request) -> IronResult<Response> {
--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,25 +1,34 @@
 // Iron crates
 extern crate iron;
 extern crate mount;
+extern crate persistent;
 
 #[macro_use]
 extern crate router;
 
-// Database dependencies
-//extern crate redis
-//extern crate mysql_simple
-//r2d2?
+// Database crates
+extern crate r2d2;
+extern crate r2d2_redis;
+extern crate redis;
+// extern crate r2d2_mysql
+// extern crate mysql_simple
 
 // Iron dependencies
 use mount::Mount;
+
+use persistent::Read;
+
 use iron::Iron;
+use iron::Chain;
 
 // Handler module
 mod handler;
+mod database;
+
+// Shared pools
+use database::RedisPool;
 
 fn main(){
-    let mut mount = Mount::new();
-
     let student_router = router!(
         get "/schedule" => handler::Enlisted::get,
         put "/schedule" => handler::Enlisted::put,
@@ -39,9 +48,18 @@ fn main(){
         get "/:subject" => handler::Subject::get
     );
 
+    println!("Connecting to database..");
+    let redis_db = database::redis_connect("redis://127.0.0.1", 512);
+    println!("Done.");
+
+    let mut mount = Mount::new();
     mount.mount("/student/", student_router);
     mount.mount("/auth/", auth_router);
     mount.mount("/subjects/", subject_router);
 
-    Iron::new(mount).listen_with("0.0.0.0:3000", 4096, iron::Protocol::Http, None).unwrap();
+    let mut chain = Chain::new(mount);
+    chain.link(Read::<RedisPool>::both(redis_db));
+
+    Iron::new(chain).listen_with("0.0.0.0:3000", 512, iron::Protocol::Http, None).unwrap();
+    println!("Started listening.");
 }
